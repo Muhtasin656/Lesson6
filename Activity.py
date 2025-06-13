5ff4462dbdbfec20bd63c1cc45939a8820c64754
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-06-13 16:16:32 +1000

    new1

---
+++ Activity.py
@@ -0,0 +1,11 @@
+a=10
+b=8
+c=3
+if a>b and c>b:
+    print("its false because one of the them are false")
+else:
+    print("false")
+if a>b or c>b:
+    print("its true because one of the them are true")
+else:
+    print("true")
